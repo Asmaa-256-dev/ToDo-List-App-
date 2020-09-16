1d65a912585f9ed738b038456dbbd576549863e9
Asmaa fathy <semofathy816@gmail.com>
2020-09-16 08:09:53 -0700

    dateTime update

--- app.js
+++ app.js
@@ -11,17 +11,30 @@ function addList(e) {
     const newLi =document.createElement ('li');
     const checkBtn =document.createElement ('button');
     const deleteBtn =document.createElement ('button');
-    
+    const newDiv=document.createElement('div');
+    newDiv.setAttribute ('id' , 'new');
+    checkBtn.setAttribute('id' , 'check');
+    deleteBtn.setAttribute('id' ,'delete');
+
+    var date= new Date();
+    var d= date.getDate();
+    var m=date.getMonth()+1;
+    var y= date.getFullYear();
+    var h= date.getHours();
+    var min = date.getMinutes();
+    var s = date.getSeconds();
+    var str01= d + "/" + m + "/" + y + " ; " + h + ":" + min + ":" + s; 
     checkBtn.innerHTML= '<i class= "fa fa-check"></i>';
     deleteBtn.innerHTML= '<i class= "fa fa-trash"></i>';
-
-
+    
     if (input.value !== '') {
         newLi.textContent= input.value;
         input.value= '';
         notCompleted.appendChild(newLi);
         newLi.appendChild(checkBtn);
         newLi.appendChild(deleteBtn);
+        newLi.appendChild(newDiv);
+        newDiv.append(str01);
     }
     checkBtn.addEventListener('click' , function() {
     const parent=this.parentNode;
@@ -33,6 +46,4 @@ function addList(e) {
         const parent=this.parentNode;
         parent.remove();
         })
-
-
 }
